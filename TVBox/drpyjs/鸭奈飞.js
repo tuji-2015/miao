85caf27ecb0fadbc6682ad66349cfd8b80c4c2da
tuji-2015 <108806492+tuji-2015@users.noreply.github.com>
2022-11-13 15:42:27 +0800

    Add files via upload

--- TVBox/drpyjs/鸭奈飞.js
+++ TVBox/drpyjs/鸭奈飞.js
@@ -1,9 +1,6 @@
 var rule = Object.assign(muban.mxpro,{
 title:'鸭奈飞',
-//host:'https://yanetflix.com',
-host:'https://yanetflix.tv/',
+host:'https://yanetflix.com',
 url:'/vodshow/fyclass--------fypage---.html',
-//class_parse:'.navbar-items&&li;a&&Text;a&&href;.*/(.*?).html',
-class_name:'电视剧&电影&综艺&动漫',
-class_url:'lianxuju&dianying&zongyi&dongman',
+class_parse:'.navbar-items li:gt(1):lt(6);a&&Text;a&&href;.*/(.*?).html',
 });

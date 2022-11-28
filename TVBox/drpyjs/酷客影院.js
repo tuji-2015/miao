22abf917b1f64c513c4b4175af4042f2e03c8310
tuji-2015 <108806492+tuji-2015@users.noreply.github.com>
2022-11-28 10:11:25 +0800

    Add files via upload

--- TVBox/drpyjs/酷客影院.js
+++ TVBox/drpyjs/酷客影院.js
@@ -12,14 +12,14 @@ var rule = {
         // "Cookie": "searchneed=ok"
     },
     //class_parse:'.stui-header__menu li.hidden-xs;a&&Text;a&&href;/(\\d+)/',
-    class_name:'电影&剧集&综艺&动漫',
-    class_url:'1&2&4&3',
+    class_name:'电影&剧集&综艺&动漫&伦理',
+    class_url:'1&2&4&3&6',
     play_parse:true,
     lazy:'',
     limit:8,
     推荐:'.stui-vodlist.clearfix;.stui-vodlist__box;a&&title;a&&data-original;.pic-text&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     一级:'.stui-vodlist.clearfix li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
-    二级:{"title":"h1.title&&Text;.stui-content__detail p:eq(0)&&Text","img":".lazyload&&data-original","content":";;;.stui-content__detail p:eq(2)&&Text;.stui-content__detail p:eq(3)&&Text","tabs":".nav.nav-tabs.pull-right&&li","lists":".stui-content__playlist:eq(#id) li"},
+    二级:{"title":"h1.line1&&Text;.stui-content__detail p:eq(0)&&Text","img":".lazyload&&data-original","desc":";;;.stui-content__detail p:eq(1)&&Text;.stui-content__detail p:eq(2)&&Text","content":".stui-content__detail p:eq(-1)&&Text","tabs":".nav.nav-tabs.pull-right&&li","lists":".stui-content__playlist:eq(#id) li"},
     
 }

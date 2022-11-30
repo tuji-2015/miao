e74f1e5e57d9b017c3556fd1fe474cbf3dd21bd5
tuji-2015 <108806492+tuji-2015@users.noreply.github.com>
2022-11-30 18:03:45 +0800

    Update py_ali.py

--- TVBox/pys/py_ali.py
+++ TVBox/pys/py_ali.py
@@ -408,7 +408,7 @@ class Spider(Spider):  # 元类 默认的元类 type
         url = 'https://api.aliyundrive.com/token/refresh'
         if len(self.authorization) == 0 or self.timeoutTick - self.localTime <= 600:
             form = {
-                'refresh_token': 'c3895f91c81741959c22feb5fe3a5def'
+                'refresh_token': '57a6d0febcba41c480eb14be6cfcb6f7'
             }
             rsp = requests.post(url, json=form, headers=self.header)
             if rsp.status_code == 200:

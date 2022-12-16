03fa98700bc0bcf4b956ddeabb0229d2a6a4860e
tuji-2015 <108806492+tuji-2015@users.noreply.github.com>
2022-12-16 17:17:01 +0800

    Add files via upload

--- TVBox/drpyjs/188看.js
+++ TVBox/drpyjs/188看.js
@@ -1,6 +1,7 @@
 var rule={
     title:'188看',
     host:'https://188kan.com',
+    // url:'/vod-show{{fl.area}}{{fl.by}}-id-{{fl.cateId}}-page-fypage{{fl.year}}.html',
 	url:'/vod-showfyfilter.html',
 	filterable:1,//是否启用分类筛选,
 	filter_url:'{{fl.area}}{{fl.by}}-id-{{fl.cateId}}-page-fypage{{fl.year}}',
@@ -16,13 +17,13 @@ var rule={
     //class_parse:'.navbar-items li:gt(1):lt(6);a&&Text;a&&href;.*/(.*?).html',
 
     searchable:2,
-    quickSearch:1,
     headers:{'User-Agent':'MOBILE_UA', },
     class_name:'电影&电视剧&综艺&动漫&纪录片',
     class_url:'1&2&3&4&5',
     推荐:'.tab-list.active;a.module-poster-item.module-item;.module-poster-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     一级:'body a.module-poster-item.module-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
-    二级:{"title":"h1&&Text;.module-info-tag&&Text","img":".lazyload&&data-original","desc":".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text;.module-info-item:eq(2)&&.module-info-item-content&&Text;.module-info-item:eq(1)&&.module-info-item-content&&Text","content":".module-info-introduction&&Text","tabs":".module-tab-item.tab-item","lists":".module-list:eq(#id)&&.module-play-list-content&&a"},
+    // 二级:{"title":"h1&&Text;.module-info-tag&&Text","img":".lazyload&&data-original","desc":".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text;.module-info-item:eq(2)&&.module-info-item-content&&Text;.module-info-item:eq(1)&&.module-info-item-content&&Text","content":".module-info-introduction&&Text","tabs":".module-tab-item.tab-item","lists":".module-list:eq(#id)&&.module-play-list-content&&a"},
+    二级:{"title":"h1&&Text;.module-info-tag&&Text","img":".lazyload&&data-original","desc":".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text;.module-info-item:eq(2)&&.module-info-item-content&&Text;.module-info-item:eq(1)&&.module-info-item-content&&Text","content":".module-info-introduction&&Text","tabs":"#y-playList .tab-item","lists":".module-play-list:eq(#id) a"},
     搜索:'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',
 }
---
+++ TVBox/drpyjs/AB影院[飞].js
@@ -0,0 +1,23 @@
+var rule = {
+     title:'AB影院',
+    host:'https://abu22.com',
+    url:'/vodtype/fyclass-fypage.html',
+    searchUrl:'/search.php#searchword=**;post',
+    headers:{ 'User-Agent':'PC_UA',},
+    class_name:'电影&剧集&综艺&动漫&伦理',
+    class_url:'1&2&4&3&40',
+    推荐:'.stui-vodlist__item;*;*;*;*',
+    一级:'.stui-vodlist__item;a&&title;a&&data-original;.pic-text&&Text;a&&href',
+   
+    二级:{
+    	"title":"h3&&Text;.stui-content__detail p:eq(0)&&Text",
+    	"img":".lazyload&&data-original",
+    	"desc":";;;.stui-content__detail p:eq(1)&&Text;.stui-content__detail p:eq(2)&&Text",
+    	// "content":".stui-content__detail p:eq(5)&&Text",
+    	"content":".stui-content__desc:eq(0)&&Text",
+    	"tabs":".stui-pannel__head.clearfix&&h3",
+    	"lists":".stui-content__playlist.clearfix:eq(#id) a"
+	},
+   
+    搜索:'ul.stui-vodlist__media&&li;*;*;*;*',
+}

0df6feaab7b3204959913efea29ee78336b8f205
tuji-2015 <108806492+tuji-2015@users.noreply.github.com>
2022-12-18 08:24:20 +0800

    Add files via upload

--- TVBox/pys/py_zhaozy.py
+++ TVBox/pys/py_zhaozy.py
@@ -1,8 +1,9 @@
-#coding=utf-8
+﻿#coding=utf-8
 #!/usr/bin/python
 import sys
 sys.path.append('..') 
 from base.spider import Spider
+from requests import session, utils
 
 class Spider(Spider):
 	def getDependence(self):
@@ -20,30 +21,48 @@ class Spider(Spider):
 	def homeContent(self,filter):
 		result = {}
 		return result
+
 	def homeVideoContent(self):
 		result = {}
 		return result
+
 	def categoryContent(self,tid,pg,filter,extend):
 		result = {}
 		return result
+
 	header = {
 		"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36",
-		"Referer": "https://zhaoziyuan.me/"
+		"Referer": "https://zhaoziyuan.la/"
 	}
+
+	cookies = ''
+	def getCookie(self):
+		header = {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36",
+			"Referer": "https://zhaoziyuan.la/login.html",
+			"Origin": "https://zhaoziyuan.la/"
+		}
+		data = {
+			'username': '用户名',
+			'password': '密码'
+		}
+		rsp = self.post('https://zhaoziyuan.la/logiu.html', data=data, headers=header)
+		self.cookies = rsp.cookies
+		return rsp.cookies
+
 	def detailContent(self,array):
 		tid = array[0]
-		print(self.getName())
 		pattern = '(https://www.aliyundrive.com/s/[^\"]+)'
 		url = self.regStr(tid,pattern)
 		if len(url) > 0:
 			return self.ali.detailContent(array)
-
-		rsp = self.fetch('https://zhaoziyuan.me/'+tid)
+		if len(self.cookies) <= 0:
+			self.getCookie()
+		rsp = self.fetch('https://zhaoziyuan.la/'+tid, cookies=self.cookies)
 		url = self.regStr(rsp.text,pattern)
 		if len(url) == 0:
 			return ""
 		newArray = [url]
-		print(newArray)
 		return self.ali.detailContent(newArray)
 
 	def searchContent(self,key,quick):
@@ -53,18 +72,21 @@ class Spider(Spider):
 		}
 		ja = []
 		for tKey in map.keys():
-			url = "https://zhaoziyuan.me/so?filename={0}&t={1}".format(key,tKey)
-			rsp = self.fetch(url,headers=self.header)
+			url = "https://zhaoziyuan.la/so?filename={0}&t={1}".format(key, tKey)
+			if len(self.cookies) <= 0:
+				self.getCookie()
+			rsp = self.fetch(url, headers=self.header, cookies=self.cookies)
 			root = self.html(self.cleanText(rsp.text))
-			aList = root.xpath("//li[@class='clear']//a")
+			aList = root.xpath("//li[@class='clear']/div/div[@class='news_text']/a")
 			for a in aList:
-				# title = a.xpath('./h3/text()')[0] + a.xpath('./p/text()')[0]
-				title = self.xpText(a,'./h3/text()') + self.xpText(a,'./p/text()')
-				pic = 'https://img0.baidu.com/it/u=603086994,1727626977&fm=253&fmt=auto?w=500&h=667'
+				title = self.xpText(a,'./h3/text()')
+				pic = 'https://inews.gtimg.com/newsapp_bt/0/13263837859/1000'
+				remark = self.xpText(a,'./p/text()').split('|')[1].strip()
 				jo = {
 					'vod_id': self.xpText(a,'@href'),
-					'vod_name': '[{0}]{1}'.format(key,title),
-					'vod_pic': pic
+					'vod_name': title,
+					'vod_pic': pic,
+					"vod_remarks": remark
 				}
 				ja.append(jo)
 		result = {

dde77f545f8746937f835bcc6b5329ffa62df04e
tuji-2015 <108806492+tuji-2015@users.noreply.github.com>
2022-12-27 10:33:52 +0800

    Add files via upload

--- TVBox/libs/drpy.js
+++ TVBox/libs/drpy.js
@@ -54,7 +54,7 @@ function pre(){
 }
 
 let rule = {};
-const VERSION = 'drpy1 3.9.30 20221204';
+const VERSION = 'drpy1 3.9.32 20221219';
 /** 已知问题记录
  * 1.影魔的jinjia2引擎不支持 {{fl}}对象直接渲染 (有能力解决的话尽量解决下，支持对象直接渲染字符串转义,如果加了|safe就不转义)[影魔牛逼，最新的文件发现这问题已经解决了]
  * Array.prototype.append = Array.prototype.push; 这种js执行后有毛病,for in 循环列表会把属性给打印出来 (这个大毛病需要重点排除一下)
@@ -1385,6 +1385,11 @@ function categoryParse(cateObj) {
     let d = [];
     // let url = cateObj.url.replaceAll('fyclass', cateObj.tid).replaceAll('fypage', cateObj.pg);
     let url = cateObj.url.replaceAll('fyclass', cateObj.tid);
+    if(cateObj.pg === 1 && url.includes('[')&&url.includes(']')){
+        url = url.split('[')[1].split(']')[0];
+    }else if(cateObj.pg > 1 && url.includes('[')&&url.includes(']')){
+        url = url.split('[')[0];
+    }
     if(rule.filter_url){
         if(!/fyfilter/.test(url)){
             if(!url.endsWith('&')&&!rule.filter_url.startsWith('&')){
@@ -1432,11 +1437,7 @@ function categoryParse(cateObj) {
             url = url.replaceAll('fypage',cateObj.pg);
         }
     }
-    if(cateObj.pg === 1 && url.includes('[')&&url.includes(']')){
-        url = url.split('[')[1].split(']')[0];
-    }else if(cateObj.pg > 1 && url.includes('[')&&url.includes(']')){
-        url = url.split('[')[0];
-    }
+
     MY_URL = url;
     // setItem('MY_URL',MY_URL);
     console.log(MY_URL);

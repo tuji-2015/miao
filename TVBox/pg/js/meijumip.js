c275058180877a2b71c36451454ca656a15f524b
tuji-2015 <108806492+tuji-2015@users.noreply.github.com>
2023-08-30 11:33:17 +0800

    Update meijumip.js

--- TVBox/pg/js/meijumip.js
+++ TVBox/pg/js/meijumip.js
@@ -4,19 +4,19 @@ var rule = {
 	//homeUrl:'/',
 	//url: '/fyclass/page/fypage/?',
 	host:'http://127.0.0.1:10078',
-	homeUrl:'/p/0/s/https://www.meijumi.net/',
-	url: '/p/0/s/https://www.meijumi.net/fyclass/page/fypage/?',
+	homeUrl:'/p/0/s/https://www.meijumi.xyz/',
+	url: '/p/0/s/https://www.meijumi.xyz/fyclass/page/fypage/?',
 	filter_url:'{{fl.class}}',
 	filter:{
 	},
-	searchUrl: '/p/0/s/https://www.meijumi.net/?s=**',
+	searchUrl: '/p/0/s/https://www.meijumi.xyz/?s=**',
 	searchable:2,
 	quickSearch:0,
 	filterable:0,
 	headers:{
 		'User-Agent': 'PC_UA',
 		'Accept': '*/*',
-		'Referer': 'https://www.meijumi.net/'
+		'Referer': 'https://www.meijumi.xyz/'
 	},
 	timeout:5000,
 	class_name:'最近更新&美剧&灵异/惊悚&魔幻/科幻&罪案/动作谍战&剧情/历史&喜剧&律政/医务&动漫/动画&纪录片&综艺/真人秀&英剧&韩剧',
@@ -39,7 +39,7 @@ let html = request(input);
 let items;
 items = pdfa(html, 'main#main div.hd ul li:has(>a>img)');
 items.forEach(it => {
-	let burl = rule.homeUrl.replace("https://www.meijumi.net/","") + pd(it, 'a&&href').replace(rule.host, "https://www.meijumi.net");
+	let burl = rule.homeUrl.replace("https://www.meijumi.xyz/","") + pd(it, 'a&&href').replace(rule.host, "https://www.meijumi.xyz");
 	d.push({
 		title: pdfh(it, 'li&&Text'),
 		desc: '',
@@ -50,7 +50,7 @@ items.forEach(it => {
 items = pdfa(html, 'main#main div.hd div.huandeng span:has(>a>img)');
 if (typeof items !== "undefined") {
 	items.forEach(it => {
-		let burl = rule.homeUrl.replace("https://www.meijumi.net/","") + pd(it, 'a&&href').replace(rule.host, "https://www.meijumi.net");
+		let burl = rule.homeUrl.replace("https://www.meijumi.xyz/","") + pd(it, 'a&&href').replace(rule.host, "https://www.meijumi.xyz");
 		d.push({
 			title: pdfh(it, 'span&&Text'),
 			desc: '',
@@ -62,7 +62,7 @@ if (typeof items !== "undefined") {
 items = pdfa(html, 'main#main div#pingbi_gg div:has(>div>a>img)');
 if (typeof items !== "undefined") {
 	items.forEach(it => {
-		let burl = rule.homeUrl.replace("https://www.meijumi.net/","") + pd(it, 'a&&href').replace(rule.host, "https://www.meijumi.net");
+		let burl = rule.homeUrl.replace("https://www.meijumi.xyz/","") + pd(it, 'a&&href').replace(rule.host, "https://www.meijumi.xyz");
 		d.push({
 			title: pdfh(it, 'a&&title'),
 			desc: pdfh(it, 'div&&span b&&Text'),
@@ -74,7 +74,7 @@ if (typeof items !== "undefined") {
 items = pdfa(html, 'main#main div#pingbi_gg div:has(>header>div>a)');
 if (typeof items !== "undefined") {
 	items.forEach(it => {
-		let burl = rule.homeUrl.replace("https://www.meijumi.net/","") + pd(it, 'header a&&href').replace(rule.host, "https://www.meijumi.net");
+		let burl = rule.homeUrl.replace("https://www.meijumi.xyz/","") + pd(it, 'header a&&href').replace(rule.host, "https://www.meijumi.xyz");
 		d.push({
 			title: pdfh(it, 'header a&&Text'),
 			desc: pdfh(it, 'header&&div span&&Text'),
@@ -93,7 +93,7 @@ if (MY_CATE !== "news" ){
 	let html = request(input);
 	let list = pdfa(html, 'div#post_list_box article');
 	list.forEach(it => {
-		let burl = rule.homeUrl.replace("https://www.meijumi.net/","") + pd(it, 'header a&&href').replace(rule.host, "https://www.meijumi.net");
+		let burl = rule.homeUrl.replace("https://www.meijumi.xyz/","") + pd(it, 'header a&&href').replace(rule.host, "https://www.meijumi.xyz");
 		d.push({
 			title: pdfh(it, 'header a&&Text'),
 			desc: pdfh(it, 'div.entry-content span:eq(1)&&Text'),
@@ -106,7 +106,7 @@ if (MY_CATE !== "news" ){
 	let html = request(input);
 	let list = pdfa(html, 'article ol&&li');
 	list.forEach(it => {
-		let burl = rule.homeUrl.replace("https://www.meijumi.net/","") + pd(it, 'a&&href').replace(rule.host, "https://www.meijumi.net");
+		let burl = rule.homeUrl.replace("https://www.meijumi.xyz/","") + pd(it, 'a&&href').replace(rule.host, "https://www.meijumi.xyz");
 		d.push({
 			title: pdfh(it, 'a&&Text'),
 			desc: pdfh(it, 'li&&span:eq(3)&&Text') + ' / 更新' + pdfh(it, 'li&&span:eq(1)&&Text'),

5e32ab5cbb135435dec0bf9563217e9e1f4fa1cc
tuji-2015 <108806492+tuji-2015@users.noreply.github.com>
2023-09-01 16:03:53 +0800

    Update 磁力熊[磁].js

--- TVBox/drpyjs/磁力熊[磁].js
+++ TVBox/drpyjs/磁力熊[磁].js
@@ -76,7 +76,7 @@ var rule = {
 		var dd=[];
 		TABS.forEach(function(tab) {
 			if (/磁力链接/.test(tab)) {
-				var d = pdfa(html, '.mv_down&&.ms-3');
+				var d = pdfa(html, '.mv_down&&.border-bottom');
 				d = d.map(function(it) {
 					var title = pdfh(it, 'a&&Text:not(:contains(详情))');
 					log('title >>>>>>>>>>>>>>>>>>>>>>>>>>' + title);

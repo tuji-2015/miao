cae04f72bc891801e2c4493278d83226075e077f
tuji-2015 <108806492+tuji-2015@users.noreply.github.com>
2023-09-05 19:53:53 +0800

    Update py_mgys.py

--- TVBox/pys/py_mgys.py
+++ TVBox/pys/py_mgys.py
@@ -228,10 +228,10 @@ class Spider(Spider):  # 元类 默认的元类 type
 		#realUrl=str(base64.b16decode(parseUrl[::-1]),'utf-8')
 		#finaUrl=realUrl[:math.floor((len(realUrl)-7)/2)] + realUrl[-(math.ceil((len(realUrl)-7)/2)):]
 		#realUrl='https://media-zjhz-fy-home.zj6oss.ctyunxs.cn/FAMILYCLOUD/b9f9fe26-e272-4e80-8a29-6bf347d86736.mp4?response-content-disposition=attachment%3Bfilename%3D%22%E8%B6%85%E5%BC%82%E8%83%BD%E6%97%8F01.mp4%22%3Bfilename*%3DUTF-8%27%27%25E8%25B6%2585%25E5%25BC%2582%25E8%2583%25BD%25E6%2597%258F01.mp4&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-CLOUDTYPEIN=CORP&x-amz-CLIENTTYPEIN=UNKNOWN&Signature=VeqzzHBvui4BKVDnoVVX0jtuAM0%3D&AWSAccessKeyId=0Lg7dAq3ZfHvePP8DKEU&Expires=1692980387&x-amz-limitrate=102400&response-content-type=video/mp4&x-amz-FSIZE=673435019&x-amz-UID=951609026966715&x-amz-UFID=11348317428886573'
-		result["parse"] = 1
-		result["playUrl"] = "" 
+		result["parse"] = 0
+		result["playUrl"] = realUrl
 		result["url"] = realUrl
-		result["header"] = json.dumps(self.headera)
+		result["header"] = ''
 		return result
 	def isVideoFormat(self,url):
 		pass

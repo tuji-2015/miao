780209160d31bfd75278d947ea226199f8865020
tuji-2015 <108806492+tuji-2015@users.noreply.github.com>
2023-09-15 23:24:14 +0800

    Update 爱看机器人.js

--- TVBox/drpyjs/爱看机器人.js
+++ TVBox/drpyjs/爱看机器人.js
@@ -18,12 +18,19 @@ try {
 	VOD.vod_content = "";
 	log(VOD);
 	var v_tks = '';
-	let script = pdfa(html1,'script').find(it=>it.includes('v_tks+=')).replace(/<script>|<\\/script>/g,'');
+	// let script = pdfa(html1,'script').find(it=>it.includes('v_tks+=')).replace(/<script>|<\\/script>/g,'');
     // print(script);
-    eval(script);
-	print('v_tks:'+v_tks);
+    // eval(script);
+	// print('v_tks:'+v_tks);
 	input = "https://www.ikanbot.com/api/getResN?videoId=" + input.split("/").pop() + "&mtype=2"+"&token="+v_tks;
-	let html = request(input,{headers: {'User-Agent':'PC_UA','Referer': input}});
+	// let html = request(input,{headers: {'User-Agent':'PC_UA','Referer': input}});
+	// let html = request(input,{headers: {'User-Agent':'MOBILE_UA','Referer': input}});
+	let html = request(input, {
+        headers: {
+            'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+            'Referer': input,
+        }
+    });
 	print(html);
 	html = JSON.parse(html);
 	let episodes = html.data.list;
@@ -38,7 +45,7 @@ try {
 			if (!playMap.hasOwnProperty(source)) {
 				playMap[source] = []
 			}
-			playMap[source].push(playurl["url"])
+			playMap[source].push(playurl["url"].replaceAll('##','#'))
 		})
 	});
 	let playFrom = [];
@@ -62,13 +69,14 @@ var rule = {
     host:'https://www.ikanbot.com',
     url:'/hot/index-fyclass-fyfilter-p-fypage.html[/hot/index-fyclass-fyfilter.html]',
     //https://www.ikanbot.com/search?q=%E6%96%97%E7%BD%97%E5%A4%A7&p=2
-    searchUrl:'/search?q=**&p=0',
+    // searchUrl:'/search?q=**&p=fypage',
+	searchUrl:'/search?q=**',
     searchable:2,
     quickSearch:0,
     filterable:1,
     filter_url:'{{fl.tag}}',
     // 图片来源:'@Referer=https://api.douban.com/@User-Agent=Mozilla/5.0%20(Windows%20NT%2010.0;%20Win64;%20x64)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/113.0.0.0%20Safari/537.36',
-    图片来源:'@Referer=https://www.ikanbot.com/@User-Agent=Mozilla/5.0%20(Windows%20NT%2010.0;%20Win64;%20x64)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/113.0.0.0%20Safari/537.36',
+	图片来源:'@Referer=https://www.ikanbot.com/@User-Agent=Mozilla/5.0%20(Windows%20NT%2010.0;%20Win64;%20x64)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/113.0.0.0%20Safari/537.36',
     filter:{
         "movie":[{"key":"tag","name":"标签","value":[{"n":"热门","v":"热门"},{"n":"最新","v":"最新"},{"n":"经典","v":"经典"},{"n":"豆瓣高分","v":"豆瓣高分"},{"n":"冷门佳片","v":"冷门佳片"},{"n":"华语","v":"华语"},{"n":"欧美","v":"欧美"},{"n":"韩国","v":"韩国"},{"n":"日本","v":"日本"},{"n":"动作","v":"动作"},{"n":"喜剧","v":"喜剧"},{"n":"爱情","v":"爱情"},{"n":"科幻","v":"科幻"},{"n":"悬疑","v":"悬疑"},{"n":"恐怖","v":"恐怖"},{"n":"治愈","v":"治愈"},{"n":"豆瓣top250","v":"豆瓣top250"}]}]
         ,"tv":[{"key":"tag","name":"标签","value":[{"n":"热门","v":"热门"},{"n":"美剧","v":"美剧"},{"n":"英剧","v":"英剧"},{"n":"韩剧","v":"韩剧"},{"n":"日剧","v":"日剧"},{"n":"国产剧","v":"国产剧"},{"n":"港剧","v":"港剧"},{"n":"日本动画","v":"日本动画"},{"n":"综艺","v":"综艺"},{"n":"纪录片","v":"纪录片"}]}]
@@ -94,14 +102,11 @@ var rule = {
     let data = {'tv': [{'key': 'tag', 'name': '标签', 'value': value}]};
     console.log(JSON.stringify(data));
     `,
-    headers:{
-        'User-Agent':'PC_UA',
-	'Referer':'https://www.ikanbot.com'
-    },
+    headers:{'User-Agent':'PC_UA',},
     class_name:'电影&剧集',
     class_url:'movie&tv',
-    play_parse:true,
-    double:true,
+	play_parse:true,
+	double:true,
     推荐:'.v-list;div.item;*;*;*;*', //这里可以为空，这样点播不会有内容
     // 一级:'.v-list&&div.item;p&&Text;img&&src;;a&&href', //一级的内容是推荐或者点播时候的一级匹配
 	一级:'.v-list&&div.item;p&&Text;img&&data-src;;a&&href', //一级的内容是推荐或者点播时候的一级匹配

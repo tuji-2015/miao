a3228bbd6f76dbfbf40905879909cda7aa518342
tuji-2015 <108806492+tuji-2015@users.noreply.github.com>
2025-04-15 15:04:15 +0800

    Add files via upload

---
+++ TVBox/js/追剧视频.js
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML>
+<!--
+	Identity by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Catvod | 主页</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta http-equiv="Cache-Control" content="no-transform">
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+		<meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no,viewport-fit=cover">
+		<meta name="renderer" content="webkit">
+		<meta http-equiv="Cache-Control" content="no-transform">
+		<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+		<meta name="theme-color" content="#0F7D00">
+		<meta name="msapplication-TileColor" content="#0F7D00">
+		<link rel="stylesheet" href="css/main.css" />
+		<noscript><link rel="stylesheet" href="css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<section id="main">
+						<header>
+							<span class="avatar"><img src="image/avatar.jpg" alt="" /></span>
+							<h1>Catvod.com</h1>
+							<p>简简单单！</p>
+						</header>
+						<footer>
+							<ul class="icons">
+								<li><a href="https://tvbox.catvod.com/" target="_blank" class="icon solid fa-blog" title="Tvbox接口"><span class="label">Tvbox接口</span></a></li>
+								<li><a href="https://github.catvod.com/" target="_blank" rel="noopener" class="icon regular fa-layer-group" title="GitHub 文件加速"><span class="label">GitHub 文件加速</span></a></li>
+								<li><a href="https://imgs.catvod.com/" target="_blank" rel="noopener" class="icon brands fa-instagram" title="随机精美壁纸"><span class="label">随机精美壁纸</span></a></li>
+								<li><a href="https://img.catvod.com/" target="_blank" rel="noopener" class="icon brands fa-instagram" title="4K随机背景图片"><span class="label">4K随机背景图片</span></a></li>
+								<li><a href="https://www.catvod.com/jiaoliu" target="_blank" rel="nofollow noopener" class="icon fas fa-envelope" title="联系我们"><span class="label">联系我们</span></a></li>
+							</ul>
+						</footer>
+					</section>
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="copyright">
+							<li>&copy; 2025 <a href="https://www.Catvod.com/" target="_blank">Catvod.com</a></li>
+						</ul>
+					</footer>
+
+			</div>
+
+			<script>
+				if ('addEventListener' in window) {
+					window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-preload\b/, ''); });
+					document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+				}
+			</script>
+			<script>
+			console.log("%c%cCatvod.com%chttps://www.Catvod.com", "line-height:28px;", "line-height:28px;padding:4px;background:#2ccbe6;color:#FADFA3;font-size:14px;", "padding:4px 4px 4px 2px;background:#ff146d;color:green;line-height:28px;font-size:12px;");
+            </script>
+	<script>(function(){function c(){var b=a.contentDocument||a.contentWindow.document;if(b){var d=b.createElement('script');d.innerHTML="window.__CF$cv$params={r:'930989dcfeab20ec',t:'MTc0NDcwMDEzOS4wMDAwMDA='};var a=document.createElement('script');a.nonce='';a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);";b.getElementsByTagName('head')[0].appendChild(d)}}if(document.body){var a=document.createElement('iframe');a.height=1;a.width=1;a.style.position='absolute';a.style.top=0;a.style.left=0;a.style.border='none';a.style.visibility='hidden';document.body.appendChild(a);if('loading'!==document.readyState)c();else if(window.addEventListener)document.addEventListener('DOMContentLoaded',c);else{var e=document.onreadystatechange||function(){};document.onreadystatechange=function(b){e(b);'loading'!==document.readyState&&(document.onreadystatechange=e,c())}}}})();</script></body>
+</html>
